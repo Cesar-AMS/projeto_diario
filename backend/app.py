564d4223e84c1c29c74b04f5adbd2294dcd49dee
cesar@cesar <sarcemandi@gmail.com>
2023-11-07 07:58:10 -0300

    adicionado codigo no app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Página Inicial'
 
+@app.route('/sobre')
+def about():
+    return 'Sobre Nós'
+
+if __name__ == '__main__':
+    app.run()
