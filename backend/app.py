39a643de76aba776cb8214faed86071380f9edbe
cesar@cesar <sarcemandi@gmail.com>
2023-11-08 11:39:19 -0300

    atualizado teste

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Página Inicial'
+    return 'Inicial'
+
 
 @app.route('/sobre')
 def about():
     return 'Sobre Nós'
+
 
 if __name__ == '__main__':
     app.run()
