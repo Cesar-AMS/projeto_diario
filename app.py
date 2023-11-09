783a10494589cc84dc902c237fdf73e728fcd3d5
cesar@cesar <sarcemandi@gmail.com>
2023-11-09 14:54:02 -0300

    atualizado comentário

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# route -> hashtagtreinamentos.com/
-# função -> o que você quer exibir naquela página
-# template
 
 
 @app.route("/")
