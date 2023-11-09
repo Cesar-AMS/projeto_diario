33db4dc32303d27c9a8deb9b4c3d655a062e3446
cesar@cesar <sarcemandi@gmail.com>
2023-11-09 09:02:17 -0300

    atualizado

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/frontend/templates/index.html')
-def index():
-    return render_template('index.html')
+def serve_index():
+    return send_from_directory('frontend', 'templates', 'index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
