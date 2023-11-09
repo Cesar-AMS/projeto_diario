345ee995757472358427128206d7080d0ef69d2f
cesar@cesar <sarcemandi@gmail.com>
2023-11-09 14:44:07 -0300

    adiocionado templates

--- app.py
+++ app.py
@@ -6,16 +6,8 @@ app = Flask(__name__)
 # template
 
 @app.route("/")
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
-
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
+def home():
+    return render_template("home.html")
 
 # colocar o site no ar
 if __name__ == "__main__":
