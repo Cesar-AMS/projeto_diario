e82c6d35e453a90e7a70d3fc3c371c8800f14ecb
cesar@cesar <sarcemandi@gmail.com>
2023-11-16 15:38:51 -0300

    adcionado classe limpar texto

--- tratacao/limpar_texto.py
+++ tratacao/limpar_texto.py
@@ -1,22 +1,27 @@
-# importaçao biblioteca
+# tratamento/limpar_texto.py
 import re
 
-# classe para limpar textos
-class Limpar_texto:
+class LimparTexto:
+    def __init__(self, padroes_remover=None):
+        """
+        Inicializa a instância da classe.
 
-    # construtor
-    def __init__(self, texto: str):
+        Args:
+        padroes_remover (list): Lista de padrões a serem removidos.
+        """
+        self.padroes_remover = padroes_remover or []
 
-        self.texto = texto
-
-    # metodo de limpar texto
-    @staticmethod
-    def limpar_texto(texto: str) -> str:
+    def limpar(self, texto):
         """
         Limpa o texto removendo caracteres especiais, pontuações, etc.
+
+        Args:
+        texto (str): O texto a ser processado.
 
         Returns:
         str: O texto limpo.
         """
-        texto_limpo = re.sub(r'[^a-zA-Z\s]', '', texto)
-        return texto_limpo
+        for padrao in self.padroes_remover:
+            texto = re.sub(padrao, '', texto)
+
+        return texto
